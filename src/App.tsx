6eb6bcab2cd07e16eca73dcab518083a17377d06
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2026-01-30 08:42:54 -0600

    move css and update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./assets/css/App.css";
 
+function ListItem({ animal }) {
+  return <li className="animal">{animal}</li>;
+}
+function List({ animals }) {
+  if (!animals) {
+    return <div>Loading...</div>;
+  }
+  if (animals.length === 0) {
+    return <div>There are no animals</div>;
+  }
+  return (
+    <ul>
+      {animals.map((animal) => {
+        return <ListItem key={animal} animal={animal}></ListItem>;
+      })}
+    </ul>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0);
-
+  const animals = ["Lion", "Sheep", "Moose"];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Animals: </h1>
+      <List animals={animals} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.js";
+import "./assets/css/index.css";
 
 const root = document.getElementById("root")
 createRoot(root).render(
